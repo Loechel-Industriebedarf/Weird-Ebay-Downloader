4d83a72ca26e7a6aedb2fe0f8ce0fe1148ed0f24
Maik Riedlsperger <maik.riedlsperger@gmail.com>
2018-01-26 12:15:09 +0100

    Initial commit

--- EMailTest/Program.cs
+++ EMailTest/Program.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿using Spire.Email;
+using Spire.Email.IMap;
+using Spire.Email.Smtp;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace EMailTest
@@ -10,6 +16,46 @@ namespace EMailTest
     {
         static void Main(string[] args)
         {
+            while (1 == 1)  //Endless loop
+            {
+                //Create an IMAP client
+                ImapClient imap = new ImapClient();
+                // Set host, username, password etc. for the client
+                imap.Host = "imap.udag.de";
+                imap.Port = 143;
+                imap.Username = "loechel-industriebedarfde-0025";
+                imap.Password = "loechel123";
+                imap.ConnectionProtocols = ConnectionProtocols.Ssl;
+                //Connect the server
+                imap.Connect();
+
+                //Select Inbox folder
+                imap.Select("Inbox");
+
+                Int64 messageCounter = imap.GetMessageCount("Inbox");
+
+                //Get the first message by its sequence number
+                MailMessage message = imap.GetFullMessage(messageCounter.ToString());
+
+                //Parse the message
+                String mailTextOriginal = message.BodyText;
+                //Cuts everything before the link
+                String mailText = mailTextOriginal.Substring(0, mailTextOriginal.LastIndexOf(">"));
+                //Cuts everything after the link
+                mailText = mailText.Substring(mailText.LastIndexOf("<"), mailText.Length - mailText.LastIndexOf("<"));
+                //Cuts first char (<)
+                mailText = mailText.Substring(1, mailText.Length - 1);
+
+                using (var client = new WebClient())
+                {
+                    System.Diagnostics.Process.Start(mailText);
+                }
+
+                Console.WriteLine(mailText);
+
+                Thread.Sleep(3888000);  //Sleep for 1.08 hours
+            }
+                
         }
     }
 }
